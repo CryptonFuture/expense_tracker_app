62a16b88ec4bd417c0c340f6bdb702e947d4ecad
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-09-07 12:30:41 +0500

    server

--- expense_tracker_backend/src/server.js
+++ expense_tracker_backend/src/server.js
@@ -29,13 +29,15 @@ app.get('/api/health', (req, res) => {
 
 // Connect to MongoDB
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI
 
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
     console.log('✅ Connected to MongoDB');
-   
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
   })
   .catch((err) => {
     console.error('❌ MongoDB connection error:', err.message);
