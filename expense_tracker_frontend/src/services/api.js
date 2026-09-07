afa713d8f8318bf291f4ea127ee1a67d8b979a9d
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-09-07 12:51:54 +0500

    server

--- expense_tracker_frontend/src/services/api.js
+++ expense_tracker_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "https://expensetrackerpython-backend.vercel.app/api",
+  baseURL: "https://expensetrackerbackend-eta.vercel.app/api",
 });
 
 API.interceptors.request.use((config) => {
